e7fa1747596f0024199bbeffc7bd8a140ec94814
ANONYMOUS <jacquesjoe01@gmail.com>
2024-09-27 12:14:09 +0200

    Initial commit avec Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'back1': "url('/img/fond1.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
